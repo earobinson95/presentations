e2620df31579d9acb7b9e6b1c6a6d4298d7fb0e4
Emily Robinson <emily.robinson@huskers.unl.edu>
2022-02-16 22:19:43 -0600

    updated slides for graphics group intergalactic time travel

--- ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/plot-generation.R
+++ ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/plot-generation.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(scales)
 library(patchwork)
 
-simulated_data  <- read_csv("ISU-Graphics-Group/2022-02-16-intergalatic-population-estimation/data/simulated-data.csv")
+simulated_data  <- read_csv("ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/data/simulated-data.csv")
 
 simulated_data %>%
   ggplot(aes(x = x, y = y, color = dataset)) +
@@ -70,4 +70,68 @@ ewokPlot <- simulated_data %>%
 
 finalPlot <- tribblePlot + ewokPlot
 finalPlot
-ggsave(finalPlot, filename = "ISU-Graphics-Group/2022-02-16-intergalatic-population-estimation/images/simulated-data-plot.jpg", width = 11, height = 5)
+ggsave(finalPlot, filename = "ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/simulated-data-plot.jpg", width = 11, height = 5)
+
+
+# Breaks
+
+baseLogPlot <- simulated_data %>%
+  filter(dataset == "dataset1") %>%
+  ggplot(aes(x = x, y = y)) +
+  geom_point() +
+  theme_bw() +
+  theme(aspect.ratio = 1,
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 12)
+  ) +
+  scale_x_continuous("Year", expand = c(0.01,0.01))
+
+p1 <- baseLogPlot + 
+  scale_y_continuous("Population",
+                     trans = "log2",
+                     limits = c(100, 55000),
+                     breaks = 2^seq(0,10000,1),
+                     labels = comma,
+                     minor_breaks = c()
+  )
+
+ggsave(p1, filename = "ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/log-no-minor-breaks.jpg", width = 4, height = 4, dpi = 300)
+
+
+p2 <- baseLogPlot + 
+  scale_y_continuous("Population",
+                     trans = "log2",
+                     limits = c(100, 55000),
+                     breaks = 2^seq(0,10000,1),
+                     labels = comma,
+                     minor_breaks = 2^seq(0,10000,1) + (2^seq(0,10000,1)/2)
+  )
+ggsave(p2, filename = "ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/log-half-minor-breaks.jpg", width = 4, height = 4)
+
+p3 <- baseLogPlot + 
+  scale_y_continuous("Population",
+                     trans = "log2",
+                     limits = c(100, 55000),
+                     breaks = 2^seq(0,10000,1),
+                     labels = comma,
+                     minor_breaks = c(2^seq(0,10000,1) + (2^seq(0,10000,1)/2), 2^seq(0,10000,1) + 3*(2^seq(0,10000,1)/4))
+  )
+ggsave(p3, filename = "ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/log-half-threequarter-minor-breaks.jpg", width = 4, height = 4)
+
+p4 <- baseLogPlot + 
+  scale_y_continuous("Population",
+                     trans = "log2",
+                     limits = c(100, 55000),
+                     breaks = 2^seq(0,10000,1),
+                     labels = comma,
+                     minor_breaks = c(2^seq(0,10000,1) + (2^seq(0,10000,1)/4), 2^seq(0,10000,1) + (2^seq(0,10000,1)/2), 2^seq(0,10000,1) + 3*(2^seq(0,10000,1)/4))
+  )
+ggsave(p4, filename = "ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/log-quarter-half-threequarter-minor-breaks.jpg", width = 4, height = 4)
+
+p5 <- baseLogPlot + 
+  scale_y_continuous("Population",
+                     trans = "log2",
+                     limits = c(100, 55000),
+                     breaks = 2^seq(0,10000,1),
+                     labels = comma)
+ggsave(p5, filename = "ISU-Graphics-Group/2022-02-16-intergalactic-population-estimation/images/log-default-minor-breaks.jpg", width = 4, height = 4)
